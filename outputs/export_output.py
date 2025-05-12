d3ff1fa73fb191b6fdffe9d340788adf49357537
IdeaPad <you@example.com>
2025-05-12 11:11:02 +0200

    added pathchoosing options to csv export

--- outputs/export_output.py
+++ outputs/export_output.py
@@ -1,11 +1,26 @@
 import os
 from datetime import datetime
 import numpy as np
+from tkinter import filedialog
+
+def choose_location():
+    filepath = filedialog.askdirectory(
+        title="Choose directory to save the file"
+    )
+    if filepath:
+        print(f"Chosen filepath: {filepath}")
+        return filepath
+
 
 def export_csv(input, parameters):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"bodeplot_{parameters[0]}Hz_{parameters[1]}Hz_{parameters[2]}Vpp_{parameters[3]}_{parameters[4]}samples_{timestamp}.csv"
-    filepath = os.path.join(os.getcwd(), filename)
+    path = choose_location()
+    if path:
+        filepath = os.path.join(path, filename)
+    else:
+        filepath = os.path.join(os.getcwd(), filename)
+        print("Using working directory as location to save")
 
     try:
         np.savetxt(filepath, input, delimiter=",", header="RMS_1,RMS_2,Frequency,Phase", comments="")
@@ -17,7 +32,6 @@ def export_csv(input, parameters):
 
 
 if __name__ == "__main__":
-        # Optional: Beispiel-Daten für Tests
     erg = []
     for i in range(200):
         result = [2, pow(3.7, i), 20000 * i, i * 10]
